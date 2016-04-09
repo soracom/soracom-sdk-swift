d251f2f5579e23ef0c489bd2f0a9e54b369d3d0c
Mason Mark <mason@fivespeed.com>
2016-04-10 07:12:47 +0900

    Add a stripped-down[1] test runner app.
    [1]: Created from the standard Xcode 7.3 Cocoa app template. Disabled code signing and removed a few unnecessary files. The purpose of this app is *only* to run the automated tests, not to be a "demo app"... we can create that separately if needed.

---
+++ Whatever/TestRunnerForSoracomSDK/TestRunnerForSoracomSDK/AppDelegate.swift
@@ -0,0 +1,12 @@
+// AppDelegate.swift TestRunnerForSoracomSDK Created by mason on 2016-04-09. Copyright © 2016 Soracom, Inc. All rights reserved.
+
+import Cocoa
+
+@NSApplicationMain
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+}
+

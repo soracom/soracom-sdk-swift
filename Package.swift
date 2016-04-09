83af51bece617ebc951db467a769c56028cb2bd0
Mason Mark <mason@fivespeed.com>
2016-04-10 07:26:42 +0900

    Add Swift Package Manager manifest file.
    As of this commit, we can build under Swift 3 pre-release (2016-03-24 edition), in addition to Swift 2.x with Xcode 7.3. However, that's only true because there are only two (very simple) source files added to the SDK so far. Swift 3 is already wildly source-incompatible with Swift 2, so we will need a dedicated Swift 3 branch pretty soon.

---
+++ Package.swift
@@ -0,0 +1,10 @@
+// Mason 2016-04-10: We will eventually be a Swift Package Manager package (Swift 3), but as of right now, the intended use case is Xcode 7.3 and Swift 2.x.
+
+import PackageDescription
+
+let package = Package(
+    name: "SoracomSDKSwift",
+    dependencies: [
+      // nope!
+    ]
+)

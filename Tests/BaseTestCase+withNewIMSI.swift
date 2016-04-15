ebce0506d578b189eec967f3d9dfa0db65f3a7dc
Mason Mark <mason@fivespeed.com>
2016-04-15 12:55:49 +0900

    Add withNewIMSI() test wrapper method for tests that operate on dummy SIM.

---
+++ Tests/BaseTestCase+withNewIMSI.swift
@@ -0,0 +1,30 @@
+// BaseTestCase+withNewIMSI.swift Created by mason on 2016-04-15. Copyright © 2016 Soracom, Inc. All rights reserved.
+
+import XCTest
+
+extension BaseTestCase {
+    
+    /// Convenience method to get a new dummy IMSI from the API sandbox, before running your actual tests. You still need to set up expectations in your main test method body. Example:
+    ///
+    ///     let expectation = beginAsyncSection()
+    ///
+    ///     withNewIMSI { (imsi) in
+    ///         // do your test here
+    ///         self.endAsyncSection()
+    ///     }
+    ///     waitForAsyncSection()
+
+    func withNewIMSI(handler: (imsi: String) -> ()) {
+        Request.createSandboxSubscriber().run { (result) in
+            XCTAssert(result.error == nil)
+            if let imsi = result.payload?[.imsi] as? String {
+                handler(imsi: imsi)
+            } else {
+                XCTFail("withNewIMSI() could not get new IMSI")
+            }
+        }
+    }
+    
+}
+
+

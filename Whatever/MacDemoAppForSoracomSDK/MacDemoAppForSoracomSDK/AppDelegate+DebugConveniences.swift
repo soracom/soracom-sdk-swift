f392acfa2e7c4815bb5721c89793c86015e1ea7b
Mason Mark <mason@fivespeed.com>
2016-05-14 23:34:15 +0900

    Move debug method stubs (intended for users to override to experiment) to a dedicated source file.

--- Whatever/MacDemoAppForSoracomSDK/MacDemoAppForSoracomSDK/AppDelegate+DebugConveniences.swift
+++ Whatever/MacDemoAppForSoracomSDK/MacDemoAppForSoracomSDK/AppDelegate+DebugConveniences.swift
@@ -1,9 +1,24 @@
-//
-//  AppDelegate+DebugConveniences.swift
-//  MacDemoAppForSoracomSDK
-//
-//  Created by mason on 2016-05-14.
-//  Copyright © 2016 Soracom, Inc. All rights reserved.
-//
+// AppDelegate+DebugConveniences.swift Created by mason on 2016-05-14. Copyright © 2016 Soracom, Inc. All rights reserved.
 
 import Foundation
+
+extension AppDelegate {
+
+    
+    @IBAction func debug1(sender: AnyObject) {
+        log("🐞 DEBUG 1: This will print the operation queue. Edit debug1() in AppDelegate.swift to change what this does.")
+        log("\(queue.operations)")
+    }
+    
+    
+    @IBAction func debug2(sender: AnyObject) {
+        log("🐜 DEBUG 2: This doesn't do anything except log this message. Edit debug2() in AppDelegate.swift to change what this does.")
+    }
+    
+    
+    @IBAction func debug3(sender: AnyObject) {
+        
+        log("🐛 DEBUG 3: This doesn't do anything except log this message. Edit debug3() in AppDelegate.swift to change what this does.")
+    }
+    
+}
--- Whatever/MacDemoAppForSoracomSDK/MacDemoAppForSoracomSDK/AppDelegate.swift
+++ Whatever/MacDemoAppForSoracomSDK/MacDemoAppForSoracomSDK/AppDelegate.swift
@@ -224,23 +224,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         log("Redaction set to \(newValue ? "ON" : "OFF").")
     }
     
-    
-    @IBAction func debug1(sender: AnyObject) {
-        log("🐞 DEBUG 1: This will print the operation queue. Edit debug1() in AppDelegate.swift to change what this does.")
-        log("\(queue.operations)")
-    }
-    
-    
-    @IBAction func debug2(sender: AnyObject) {
-        log("🐜 DEBUG 2: This doesn't do anything except log this message. Edit debug2() in AppDelegate.swift to change what this does.")
-    }
-    
-    
-    @IBAction func debug3(sender: AnyObject) {
-        
-        log("🐛 DEBUG 3: This doesn't do anything except log this message. Edit debug3() in AppDelegate.swift to change what this does.")
-    }
-    
 }
 
 

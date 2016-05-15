19464672b1859da9c50e5df2e08457f8b9256524
Mason Mark <mason@fivespeed.com>
2016-05-15 19:21:53 +0900

    Add RequestQueue, which provides an optional default operation queue for sequencing API requests.

---
+++ Sources/RequestQueue.swift
@@ -0,0 +1,20 @@
+// RequestQueue.swift Created by mason on 2016-05-08. Copyright © 2016 Soracom, Inc. All rights reserved.
+
+import Foundation
+
+/// Implements a singleton operation queue used that may be used to run all requests in a client app, if that's convenient.
+///
+/// It is not necessary to use this; a client app is free to define its own operation queue(s) as needed.
+
+
+class RequestQueue {
+    
+    /// Returns a single shared NSOperationQueue instance, that may be used to run all requests, if that's convenient.
+    
+    static let sharedQueue: NSOperationQueue = {
+        let q = NSOperationQueue()
+        q.maxConcurrentOperationCount = 1
+        return q
+    }()
+    
+}

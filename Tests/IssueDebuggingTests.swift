0fae594b5a6569d253f15357d858e96d6babe052
Mason Mark <mason@soracom.jp>
2016-08-01 16:55:52 +0900

    Add a generic test case for helping to debug other projects' issues.

---
+++ Tests/IssueDebuggingTests.swift
@@ -0,0 +1,28 @@
+// IssueDebuggingTests.swift Created by Mason Mark on 7/29/16. Copyright © 2016 Soracom, Inc. All rights reserved.
+
+import XCTest
+
+
+/// These are not really tests of this SDK, they are test cases that use this SDK to help debug issues in various other projects that use the Soracom API. (But using this SDK for that purpose may result in improvements or bug fixes to it as well.)
+
+class IssueDebuggingTests: BaseTestCase {
+    
+    func test_bro() {
+    
+        // https://api-sandbox.soracom.io/v1/groups/c8726c12-75f2-47db-8ecb-ee35d6e9ee66/subscribers?status_filter=suspended&limit=10
+        
+        let req = Request.listSubscribersInGroup("c8726c12-75f2-47db-8ecb-ee35d6e9ee66")
+        
+        req.query = Request.makeQueryDictionary(statusFilter: [.inactive])
+        let res = req.wait();
+        
+        let subscribers = Subscriber.listFrom(res.payload)
+        
+        print(req)
+        print(res)
+        print(subscribers)
+        print("subscribers count is \(subscribers?.count)")
+        
+        print("w00t?")
+    }
+}

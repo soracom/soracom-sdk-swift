cd54eb19c36439de9da412fcfcecd2d08915ab3e
Mason Mark <mason@soracom.jp>
2017-02-14 15:58:10 +0900

    Stub out a nonfunctional Keychain class for Linux. Now builds; doesn't work yet.
    We probably won't have a real Keychain on Linux and will just write a file or dir and punt the security responsibility onto the user ;-?
    
    Anyway that is deferred; at this point I am just trying to get it to _build_ so that we can itemize all the remaining work to support Linux.

--- Sources/Keychain.swift
+++ Sources/Keychain.swift
@@ -1,117 +1,148 @@
 // Keychain.swift Created by mason on 2016-02-21. Copyright © 2016 masonmark.com. All rights reserved.
 
 import Foundation
-import Security
 
 
-/// Simplest possible wrapper for keychain read/write. (Be aware that this **overwrites** any existing value for key when it writes, by design).
-
-open class Keychain {
+#if os(Linux)
     
-    /// Find and return a blob of data previously stored under `key` using this class's `write()` method. Returns nil if not found. This is the primitive read method.
-    
-    open static func read(_ key: String) -> Data? {
-        guard key != "" else {
-            return nil; // because actually querying with empty string key returns something weird
+    open class Keychain {
+        
+        open static func read(_ key: String) -> Data? {
+            return nil
         }
         
-        let query: [String:Any] = [
-            kSecClass as String       : kSecClassGenericPassword,
-            kSecAttrAccount as String : key,
-            kSecReturnData as String  : kCFBooleanTrue,
-            kSecMatchLimit as String  : kSecMatchLimitOne
-        ]
-        
-        var dataTypeRef: AnyObject?
-        let status = withUnsafeMutablePointer(to: &dataTypeRef) { SecItemCopyMatching(query as CFDictionary, UnsafeMutablePointer($0)) }
-        
-        if status == errSecSuccess {
-            if let data = dataTypeRef as! Data? {
-                return data
-            }
-        } else if status == errSecItemNotFound {
-            // don't log this by default, even if logging is on; it's an expected condition in many circumstances
-        } else {
-            errorLogger(status, "read item \(key)")
+        open static func write(_ key: String, data: Data) -> Bool {
+            return false
         }
-        return nil
+        
+        open static func delete(_ key: String) -> Bool {
+            return false
+        }
     }
     
+    public typealias OSStatus = Int32
     
-    /// Store `data` to the Keychain, under `key`, **overwriting** any existing value. Returns true on success, false on error. This is the primitive write method.
     
-    open static func write(_ key: String, data: Data) -> Bool {
-        let query: [String:Any] = [
-            kSecClass as String       : kSecClassGenericPassword as String,
-            kSecAttrAccount as String : key,
-            kSecValueData as String   : data
-        ]
+#elseif os(macOS) || os(iOS)
+    
+    import Security
+    
+    
+    /// Simplest possible wrapper for keychain read/write. (Be aware that this **overwrites** any existing value for key when it writes, by design).
+    
+    open class Keychain {
         
-        let deleteResult = SecItemDelete(query as CFDictionary)
-        if deleteResult != noErr && deleteResult != errSecItemNotFound {
-            // not found is a normal occurence
+        /// Find and return a blob of data previously stored under `key` using this class's `write()` method. Returns nil if not found. This is the primitive read method.
+        
+        open static func read(_ key: String) -> Data? {
+            guard key != "" else {
+                return nil; // because actually querying with empty string key returns something weird
+            }
             
-            errorLogger(deleteResult, "delete/overwrite item \(key)")
-
-            #if os(OSX)
-                let errMessage = SecCopyErrorMessageString(deleteResult, nil)
-                errorLogger(deleteResult, "error description: \(String(describing: errMessage))")
-                
-                // Mason 2016-04-18: I am seeing -25244 "Invalid attempt to change the owner of this item" during unit tests.
-                // Some debuggery reveals that this is because the SDK unit tests are being run by multiple different
-                // apps, but the tests use the same keychain keys. This is not something this simple Keychain wrapper supports.
-                // I fixed this in the tests by uniquing the keys per-app, but am leaving this note for posterity.
-            #endif
+            let query: [String:Any] = [
+                kSecClass as String       : kSecClassGenericPassword,
+                kSecAttrAccount as String : key,
+                kSecReturnData as String  : kCFBooleanTrue,
+                kSecMatchLimit as String  : kSecMatchLimitOne
+            ]
             
-            if deleteResult == -34018 {
+            var dataTypeRef: AnyObject?
+            let status = withUnsafeMutablePointer(to: &dataTypeRef) { SecItemCopyMatching(query as CFDictionary, UnsafeMutablePointer($0)) }
+            
+            if status == errSecSuccess {
+                if let data = dataTypeRef as! Data? {
+                    return data
+                }
+            } else if status == errSecItemNotFound {
+                // don't log this by default, even if logging is on; it's an expected condition in many circumstances
+            } else {
+                errorLogger(status, "read item \(key)")
+            }
+            return nil
+        }
+        
+        
+        /// Store `data` to the Keychain, under `key`, **overwriting** any existing value. Returns true on success, false on error. This is the primitive write method.
+        
+        open static func write(_ key: String, data: Data) -> Bool {
+            let query: [String:Any] = [
+                kSecClass as String       : kSecClassGenericPassword as String,
+                kSecAttrAccount as String : key,
+                kSecValueData as String   : data
+            ]
+            
+            let deleteResult = SecItemDelete(query as CFDictionary)
+            if deleteResult != noErr && deleteResult != errSecItemNotFound {
+                // not found is a normal occurence
                 
-                if NSClassFromString("XCTest") != nil {
-                    let gameOverMan = [
-                        "💀 SOMEBODY SET US UP THE XCODE BOMB.",
-                        "💀 Sadly, this sucks. Xcode has an issue.",
-                        "💀 This may help: https://forums.developer.apple.com/thread/4743.",
-                        "💀 (find for 'Mason' in that thread). TL;DR: enable Keychain Sharing",
-                        "💀 entitlement for your main app, even if you don't need it.",
-                        "💀 Please file a bug with Apple if this happens on your machine...",
-                    ].joined(separator: "\n")
+                errorLogger(deleteResult, "delete/overwrite item \(key)")
+                
+                #if os(OSX)
+                    let errMessage = SecCopyErrorMessageString(deleteResult, nil)
+                    errorLogger(deleteResult, "error description: \(String(describing: errMessage))")
                     
-                    fatalError(gameOverMan)
+                    // Mason 2016-04-18: I am seeing -25244 "Invalid attempt to change the owner of this item" during unit tests.
+                    // Some debuggery reveals that this is because the SDK unit tests are being run by multiple different
+                    // apps, but the tests use the same keychain keys. This is not something this simple Keychain wrapper supports.
+                    // I fixed this in the tests by uniquing the keys per-app, but am leaving this note for posterity.
+                #endif
+                
+                if deleteResult == -34018 {
+                    
+                    if NSClassFromString("XCTest") != nil {
+                        let gameOverMan = [
+                            "💀 SOMEBODY SET US UP THE XCODE BOMB.",
+                            "💀 Sadly, this sucks. Xcode has an issue.",
+                            "💀 This may help: https://forums.developer.apple.com/thread/4743.",
+                            "💀 (find for 'Mason' in that thread). TL;DR: enable Keychain Sharing",
+                            "💀 entitlement for your main app, even if you don't need it.",
+                            "💀 Please file a bug with Apple if this happens on your machine...",
+                            ].joined(separator: "\n")
+                        
+                        fatalError(gameOverMan)
+                    }
                 }
             }
+            
+            let status: OSStatus = SecItemAdd(query as CFDictionary, nil)
+            
+            if status != noErr && status != errSecItemNotFound {
+                errorLogger(status, "add item \(key)")
+            }
+            return status == noErr
         }
         
-        let status: OSStatus = SecItemAdd(query as CFDictionary, nil)
-
-        if status != noErr && status != errSecItemNotFound {
-            errorLogger(status, "add item \(key)")
+        
+        /// Delete the blob of data stored under `key`, if any.
+        
+        open static func delete(_ key: String) -> Bool {
+            let query: [String:Any] = [
+                kSecClass as String       : kSecClassGenericPassword,
+                kSecAttrAccount as String : key
+            ]
+            
+            let status: OSStatus = SecItemDelete(query as CFDictionary)
+            
+            if status != noErr && status != errSecItemNotFound {
+                errorLogger(status, "delete item \(key)")
+            }
+            
+            return status == noErr
         }
-        return status == noErr
     }
     
+    // Thanks, Obama: https://gist.github.com/jackreichert/414623731241c95f0e20
     
-    /// Delete the blob of data stored under `key`, if any.
-    
-    open static func delete(_ key: String) -> Bool {
-        let query: [String:Any] = [
-            kSecClass as String       : kSecClassGenericPassword,
-            kSecAttrAccount as String : key
-        ]
-        
-        let status: OSStatus = SecItemDelete(query as CFDictionary)
-        
-        if status != noErr && status != errSecItemNotFound {
-            errorLogger(status, "delete item \(key)")
-        }
+#endif
 
-        return status == noErr
-    }
-    
+
+extension Keychain {
     
     /// Convenience method to read a UTF-8 string.
     
     open static func readString(_ key: String) -> String? {
-        if let data = read(key), let stringValue = NSString(data: data, encoding: String.Encoding.utf8.rawValue) {
-            return stringValue as String
+        if let data = read(key), let stringValue = String(data: data, encoding: .utf8) {
+            return stringValue
         } else {
             return nil
         }
@@ -128,7 +159,7 @@ open class Keychain {
         }
     }
     
-
+    
     /// Because Keychain in OS X and iOS has had some gnarly-ass bugs over the years, and still might, log errors. This default logger just uses print() but you can replace it with your own logger if you want.
     
     static var errorLogger: KeychainErrorLogger = {
@@ -137,15 +168,13 @@ open class Keychain {
         
         var errCodeDescription = ""
         #if os(OSX)
-            print(SecCopyErrorMessageString(errCode, nil) ?? "unspecified error description")
+            print(SecCopyErrorMessageString(errCode, nil) ?? "SecCopyErrorMessageString() couldn't return the error message.")
         #endif
-
-    
+        
+        
         print("KEYCHAIN ERROR: An error occurred when trying to \(whenTryingTo): \(errCode)")
         // Mason 2016-08-17: I don't know of a reliable way to go from errCode to the corresponding symbol name on iOS.
     }
     
     public typealias KeychainErrorLogger = ((_ errCode: OSStatus, _ whenTryingTo: String ) -> Void)
 }
-
-// Thanks, Obama: https://gist.github.com/jackreichert/414623731241c95f0e20

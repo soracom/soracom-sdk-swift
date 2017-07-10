716b139cb9044e5b57611315c2ee9546702e5f76
Mason Mark <mason@soracom.jp>
2017-07-10 17:52:36 +0900

    Upgrade LinuxDemoAppForSoracomSDK to use Swift tools v4, for new manifest format
    This lets us use the new (in Swift 4) feature to depend on a branch.
    
    This builds, but (as expected) doesn't yet work:
    -----
    ubuntu@mason-linux-2017:~/Code/SORACOM/soracom-sdk-swift/Whatever/LinuxDemoAppForSoracomSDK$ swift build
    Compile Swift Module 'SoracomSDKSwift' (27 sources)
    Compile Swift Module 'LinuxDemoAppForSoracomSDK' (1 sources)
    Linking ./.build/debug/LinuxDemoAppForSoracomSDK
    ubuntu@mason-linux-2017:~/Code/SORACOM/soracom-sdk-swift/Whatever/LinuxDemoAppForSoracomSDK$ .build/debug/LinuxDemoAppForSoracomSDK
    Hello, world!
    oldValue is 86400
    fatal error: work in progress bro (FIXME) 86400: file /mnt/hgfs/Code/SORACOM/soracom-sdk-swift/Whatever/LinuxDemoAppForSoracomSDK/.build/checkouts/soracom-sdk-swift-4772109555537792426/Sources/Payload.swift, line 210
    Current stack trace:
    0    libswiftCore.so                    0x00007f8c25f7e960 _swift_stdlib_reportFatalErrorInFile + 221
    1    libswiftCore.so                    0x00007f8c25c7ae90 closure #1 in closure #1 in closure #1 in _assertionFailure(_:_:file:line:flags:) + 284
    2    libswiftCore.so                    0x00007f8c25f2ad31 <unavailable> + 4173105
    3    libswiftCore.so                    0x00007f8c25f3d999 <unavailable> + 4250009
    4    libswiftCore.so                    0x00007f8c25c7a76b <unavailable> + 1353579
    5    libswiftCore.so                    0x00007f8c25eb6a30 specialized closure #1 in _assertionFailure(_:_:file:line:flags:) + 169
    6    libswiftCore.so                    0x00007f8c25eebfef <unavailable> + 3915759
    7    libswiftCore.so                    0x00007f8c25c7a76b <unavailable> + 1353579
    8    libswiftCore.so                    0x00007f8c25e4d2b0 specialized _assertionFailure(_:_:file:line:flags:) + 144
    9    LinuxDemoAppForSoracomSDK          0x000000000044975c <unavailable> + 300892
    10   LinuxDemoAppForSoracomSDK          0x0000000000449e8f <unavailable> + 302735
    11   LinuxDemoAppForSoracomSDK          0x000000000044c44d <unavailable> + 312397
    12   LinuxDemoAppForSoracomSDK          0x0000000000467560 <unavailable> + 423264
    13   LinuxDemoAppForSoracomSDK          0x0000000000464d88 <unavailable> + 413064
    14   LinuxDemoAppForSoracomSDK          0x0000000000466625 <unavailable> + 419365
    15   LinuxDemoAppForSoracomSDK          0x00000000004086ff <unavailable> + 34559
    16   libc.so.6                          0x00007f8c23e46740 __libc_start_main + 240
    17   LinuxDemoAppForSoracomSDK          0x00000000004080f9 <unavailable> + 33017
    Illegal instruction (core dumped)
    ubuntu@mason-linux-2017:~/Code/SORACOM/soracom-sdk-swift/Whatever/LinuxDemoAppForSoracomSDK$

--- Whatever/LinuxDemoAppForSoracomSDK/Package.swift
+++ Whatever/LinuxDemoAppForSoracomSDK/Package.swift
@@ -1,9 +1,19 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "LinuxDemoAppForSoracomSDK",
     dependencies: [
-    .Package(url: "../..", majorVersion: 0),
-
+        .package(url: "../..", .branch( "add-linux-support") ),
+    ],
+    targets: [
+        .target(
+            name: "LinuxDemoAppForSoracomSDK", 
+            dependencies: [
+                "SoracomSDKSwift"
+            ],
+            path: "Sources"
+        )
     ]
 )

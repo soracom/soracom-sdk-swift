c8b9bd1c503258a16c9aa1a78fc9217256209e0d
Mason Mark <mason@soracom.jp>
2017-07-10 18:24:21 +0900

    Update package manifest for swift-tools-version:4.0

---
+++ LinuxMain.swift
@@ -0,0 +1,7 @@
+// LinuxMain.swift Created by mason on 2017-07-10. Copyright © 2017 Soracom, Inc. All rights reserved.
+
+import Foundation
+
+class <# WHUTWHUT #> {
+    
+}
--- Package.swift
+++ Package.swift
@@ -1,11 +1,22 @@
+// swift-tools-version:4.0
+
 // Mason 2016-04-10: We will eventually be a Swift Package Manager package (Swift 3), but as of right now, the intended use case is Xcode 7.3 and Swift 2.x.
 
 import PackageDescription
 
 let package = Package(
     name: "SoracomSDKSwift",
-    dependencies: [
-      // nope!
+    targets: [
+        .target(
+            name: "SoracomSDKSwift",
+            dependencies: [],
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "SoracomSDKSwiftTests",
+            dependencies: [],
+            path: "Tests"
+        )
     ]
 )
-package.exclude = ["Whatever"]
+

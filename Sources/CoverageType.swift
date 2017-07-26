07f9ba657a36b8d682c6dd0d532fc28e8200656a
Mason Mark <mason@soracom.jp>
2017-07-26 17:21:09 +0900

    Move CoverageType to its own source file, and delete poorly-organized APIStructures.swift

---
+++ Sources/CoverageType.swift
@@ -0,0 +1,10 @@
+// CoverageType.swift Created by mason on 2017-07-26. 
+
+/// Defines all valid coverage types
+
+public enum CoverageType: String {
+    case japan  = "jp"
+    case global = "g"
+}
+
+

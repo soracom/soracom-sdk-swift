0bc547dec1581fcd2ce25db910976a90dc4417cc
Mason Mark <mason@soracom.jp>
2018-08-14 12:05:39 -0700

    Remove obsolete non-autogenerated model

--- Sources/SoracomAPI/AirStats.swift
+++
@@ -1,9 +0,0 @@
-// AirStats.swift Created by mason on 2017-07-26. 
-
-/// Description forthcoming.
-
-public struct AirStats:  Codable {
-    
-    var dataTrafficStatsMap: DataTrafficStatsMap
-    var unixtime: Int    
-}

41b48461fc482165434a0dd35696555146591410
Mason Mark <mason@soracom.jp>
2018-08-21 16:41:11 -0700

    Implement ExpressibleByDictionaryLiteral for TagUpdateRequrest & [TagUpdateRequrest]

--- Sources/SoracomAPI/Models/TagUpdateRequest+Extras.swift
+++ Sources/SoracomAPI/Models/TagUpdateRequest+Extras.swift
@@ -1,8 +1,27 @@
-//
-//  TagUpdateRequest+Extras.swift
-//  SoracomAPI
-//
-//  Created by Mason Mark on 2018-08-21.
-//
+// TagUpdateRequest+Extras.swift Created by Mason Mark on 2018-08-21.
 
-import Foundation
+/// Convenience extension to make initializing TagUpdateRequest ( and [TagUpdateRequest]) nicer.
+
+extension TagUpdateRequest: ExpressibleByDictionaryLiteral {
+    public convenience init(dictionaryLiteral elements: (String, String)...) {
+        self.init(tagName: elements[0].0, tagValue: elements[0].1)
+    }
+    
+    public typealias Key = String
+    public typealias Value = String
+}
+
+extension Array: ExpressibleByDictionaryLiteral where Element == TagUpdateRequest {
+    
+    public init(dictionaryLiteral elements: (String, String)...) {
+        self.init()
+        for (k,v) in elements {
+            append([k:v])
+        }
+    }
+    
+    public typealias Key = String
+    
+    public typealias Value = String
+    
+}
--- Tests/SoracomAPITests/TagUpdateRequestTests.swift
+++ Tests/SoracomAPITests/TagUpdateRequestTests.swift
@@ -1,8 +1,40 @@
-//
-//  TagUpdateRequestTests.swift
-//  SoracomAPITests
-//
-//  Created by Mason Mark on 2018-08-21.
-//
+// TagUpdateRequestTests.swift Created by Mason Mark on 2018-08-21. Copyright © 2018 Soracom, Inc. All rights reserved.
 
-import Foundation
+import XCTest
+
+#if USE_TESTABLE_IMPORT_FOR_MAC_DEMO_APP
+    // Do nothing (it's magic). We unfortunately need 3 different import
+    // modes: Xcode+macOS, Xcode+iOS, and non-Xcode ("swift test" CLI)
+    // due to macOS and iOS not supporting SPM build/test...
+#elseif USE_TESTABLE_IMPORT_FOR_IOS_DEMO_APP
+    @testable import iOSDemoAppForSoracomSDK
+#else
+    @testable import SoracomAPI
+#endif
+
+class TagUpdateRequestTests: XCTestCase {
+    
+    func test_convenience_initializers() {
+        
+        let foo: TagUpdateRequest = ["key1": "value1"]
+        XCTAssert(foo.tagName == "key1")
+        XCTAssert(foo.tagValue == "value1")
+        
+        let bar: [TagUpdateRequest] = ["key2": "value2", "key3": "value3"]
+        XCTAssert(bar[0].tagName == "key2")
+        XCTAssert(bar[0].tagValue == "value2")
+        XCTAssert(bar[1].tagName == "key3")
+        XCTAssert(bar[1].tagValue == "value3")
+    }
+}
+
+#if os(Linux)
+extension TagUpdateRequestTests {
+    static var allTests : [(String, (TagUpdateRequestTests) -> () throws -> Void)] {
+        return [
+            ("test_convenience_initializers", test_convenience_initializers),
+        ]
+    }
+}
+#endif
+

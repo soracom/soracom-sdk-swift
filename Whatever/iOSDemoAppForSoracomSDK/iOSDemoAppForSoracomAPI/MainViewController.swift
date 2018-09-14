2435128c83c0950d59b8ca1fb4311df6a3f23391
Christian Inkster <christian.inkster@gmail.com>
2018-09-14 18:28:13 +0800

    Join together

--- Whatever/iOSDemoAppForSoracomSDK/iOSDemoAppForSoracomAPI/MainViewController.swift
+++ Whatever/iOSDemoAppForSoracomSDK/iOSDemoAppForSoracomAPI/MainViewController.swift
@@ -4,28 +4,51 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    private var sandboxMode = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Cleans any perhaps expired tokens and removes sandbox user
+        self.cleanStart()
         
         self.title = "SORACOM SDK Demo"
         
         let nc = NotificationCenter.default
         
         nc.addObserver(forName: Notifications.SandboxUserAuthenticationDidUpdate, object: nil, queue: nil) { (notif) in
-            
-            self.authStatusField.text  = Client.sharedInstance.sandboxUserAuthenticationStatus
-            self.helpMessageField.text = Client.sharedInstance.helpMessage
+            self.updateDisplay()
         }
         
-        authStatusField.text = Client.sharedInstance.sandboxUserAuthenticationStatus
-        helpMessageField.text = Client.sharedInstance.helpMessage
+        self.updateDisplay()
 
+    }
+    
+    func updateDisplay(){
+        self.authStatusField.text  = Client.sharedInstance.sandboxUserAuthenticationStatus
+        self.helpMessageField.text = Client.sharedInstance.helpMessage
+        
+        if (self.sandboxMode){
+            let cred = Client.sharedInstance.credentialsForSandboxUser
+            self.viewSubscribersButton.isHidden = (cred.token == "")
+            self.authButton.isHidden = (cred.token != "")
+        } else {
+            let cred = Client.sharedInstance.credentialsForProductionSAMUser
+            self.viewSubscribersButton.isHidden = (cred.token == "")
+            self.authButton.isHidden = (cred.token != "")
+        }
+        self.sandboxSwitch.isOn = self.sandboxMode
     }
     
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         print("\(self): prepareForSegue: \(segue)")
+        if (segue.identifier == "ShowSubscribers"){
+            guard let dest = segue.destination as? SubscribersTableViewController else {
+                return
+            }
+            dest.sandboxMode = self.sandboxMode
+        }
     }
 
     
@@ -34,14 +57,77 @@ class MainViewController: UIViewController {
     }
  
     
+    @IBOutlet weak var sandboxSwitch: UISwitch!
     @IBOutlet weak var authStatusField:  UILabel!
     @IBOutlet weak var helpMessageField: UILabel!
-
+    @IBOutlet weak var viewSubscribersButton: UIButton!
+    @IBOutlet weak var authButton: UIButton!
+    
+    @IBAction func sandboxModeChanged(_ sender: UISwitch) {
+        if (sender.isOn){
+            BaseRequest.endpointHost = BaseRequest.sandboxEndpointHost
+            BaseRequest.credentialsFinder = { (req) in
+                return Client.sharedInstance.credentialsForSandboxUser
+            }
+        } else {
+            BaseRequest.endpointHost = BaseRequest.productionEndpointHost
+            BaseRequest.credentialsFinder = { (req) in
+                return Client.sharedInstance.credentialsForProductionSAMUser
+            }
+        }
+        self.sandboxMode = sender.isOn
+        self.updateDisplay()
+    }
     
     @IBAction func startButtonPressed(sender: AnyObject) {
         
-        Client.sharedInstance.authenticateAsSandboxUser(true)
+        if (self.sandboxMode){
+            Client.sharedInstance.authenticateAsSandboxUser(true)
+        } else {
+            self.performProductionAuth()
+        }
         // trigger auth and update the status display
+    }
+    
+    func performProductionAuth(){
+        
+        var credentials = Client.sharedInstance.credentialsForProductionSAMUser
+        let authReq = Request.auth(credentials) { (response) in
+            print("Auth: \(response)")
+            if let authResponse = response.parse(),
+                let apiKey = authResponse.apiKey,
+                let token  = authResponse.token
+            {
+                credentials.apiKey = apiKey
+                credentials.token  = token
+                _ = credentials.save()
+                Client.sharedInstance.saveCredentials(credentials, user: .ProductionSAMUser)
+                
+                self.authStatusField.text = "🆗 Authenticated as: \(credentials.apiKey)"
+                self.helpMessageField.text = ""
+                self.updateDisplay()
+                
+            } else {
+                
+                let message = response.error?.message ?? "generic error"
+                self.authStatusField.text = "⚠️ Failed: \(message)"
+                self.helpMessageField.text = "(Valid SAM user credentials from the production environment are required.)"
+                self.updateDisplay()
+
+            }
+            
+        }
+        authReq.run()
+    }
+    
+    func cleanStart(){
+        var credentials = Client.sharedInstance.credentialsForProductionSAMUser
+        credentials.token  = ""
+        _ = credentials.save()
+        Client.sharedInstance.saveCredentials(credentials, user: .ProductionSAMUser)
+        
+        // Completely nuke sandbox user
+        Client.sharedInstance.saveCredentials(SoracomCredentials(), user: .APISandboxUser)
     }
 
 }
--- Whatever/iOSDemoAppForSoracomSDK/iOSDemoAppForSoracomAPI/SubscribersTableViewController.swift
+++ Whatever/iOSDemoAppForSoracomSDK/iOSDemoAppForSoracomAPI/SubscribersTableViewController.swift
@@ -10,37 +10,80 @@ import UIKit
 
 class SubscribersTableViewController: UITableViewController {
 
+    var sandboxMode = true
+    var subscribers: SubscriberList = []
+    
+    fileprivate func loadSubscribers() {
+        let req = Request.listSubscribers()
+        
+        req.responseHandler = { (response) in
+            if let error = response.error {
+                print("Error: \(response) \(error)")
+            } else {
+                if let subs = response.parse(){
+                    print("Subs: \(subs)")
+                    self.subscribers = subs
+                    self.tableView.reloadData()
+                } else {
+                    print("Couldnt extract subs")
+                }
+            }
+        }
+        
+        req.run()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        loadSubscribers()
+        
+        if (sandboxMode){
+            let addbutton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonPressed(sender:)))
+            self.navigationItem.rightBarButtonItem = addbutton
+        }
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("Appearing!")
+        self.tableView.reloadData()
+    }
 
+    @objc func addButtonPressed( sender: UIBarButtonItem ){
+        Client.sharedInstance.createSandboxSIM(){
+            (result) in
+            if result {
+                self.loadSubscribers()
+            }
+        }
+    }
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.subscribers.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SubscriberCell", for: indexPath)
 
-        // Configure the cell...
+        let sub = self.subscribers[indexPath.row]
+        
+        cell.textLabel?.text = sub.imsi
+        cell.detailTextLabel?.text = "\(sub.status ?? "Unknown") \(sub.speedClass ?? "Unknown")"
 
         return cell
     }
-    */
+ 
 
     /*
     // Override to support conditional editing of the table view.
@@ -77,14 +120,20 @@ class SubscribersTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        guard let dest = segue.destination as? SubscriberDetailTableViewController, let indexPath = self.tableView.indexPathForSelectedRow else {
+            return
+        }
+        
+        dest.sub = self.subscribers[indexPath.row]
+        
     }
-    */
+ 
 
 }
